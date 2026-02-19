e355e1caf626a6bf1c52afd29eb7d9d8906613bf
Gabriele <gabriele8.gb@gmail.com>
2026-02-19 18:02:18 +0100

    ffff

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
   <?php wp_head(); ?>
 </head>
-<body <? body_class(); ?>>
+<body <?php body_class(); ?>>
     
   <header>
   <div class="container">
