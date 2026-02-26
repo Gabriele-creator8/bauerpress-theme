413d29f6620bdc9fdebea0e763fb3b5c75bb8276
Gabriele <gabriele8.gb@gmail.com>
2026-02-26 14:52:46 +0100

    Modifiche_26022026_14:52

--- single.php
+++ single.php
@@ -1,36 +1,48 @@
-<?php
-/**
- * Template per il singolo articolo (single.php)
- */
-get_header ();
-?>
+<?php get_header(); ?>
 
 <main class="site-main container">
-    <section>
-        <?php if ( have_posts () ) : while ( have_posts() ) : the_post(); ?>
-        
-            <article <?php post_class(); ?>>
-                <header class="single-post-header">
-                    <h1 class="single-post-title"><?php the_title(); ?></h1>
-                    <p class="single-post-meta">
-                        Pubblicato il <?php echo get_the_date(); ?>
-                        <?php $cats = get_the_category();
-                        if ( $cats ) : ?>
-                            in <span class="badge"><?php echo esc_html( $cats[0]->name); ?></span>
-                        <?php endif; ?>
-                    </p>
-                </header>
-                <div class="single-post-content">
-                    <?php the_content(); ?>
-                </div>
-            </article>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <?php comments_template(); ?>
-        
-        <?php endwhile; endif; ?>
-    </section>
+<article <?php post_class(); ?>>
 
-    <?php get_sidebar(); ?>
+  <header class="single-post-header">
+
+    <h1 class="single-post-title"><?php the_title(); ?></h1>
+
+    <p class="single-post-meta">
+      Pubblicato il <?php echo get_the_date(); ?>
+      da <strong><?php the_author(); ?></strong>
+
+      <?php $cats = get_the_category();
+      if ( $cats ) : ?>
+        in <span class="badge"><?php echo esc_html( $cats[0]->name ); ?></span>
+      <?php endif; ?>
+    </p>
+
+    <?php if ( function_exists('get_field') ) : ?>
+
+      <?php if ( get_field('luogo') ) : ?>
+        <p>Luogo dell'evento: <?php the_field('luogo'); ?></p>
+      <?php endif; ?>
+
+      <?php if ( get_field('data_evento') ) : ?>
+        <p>Data dell'evento: <?php the_field('data_evento'); ?></p>
+      <?php endif; ?>
+
+    <?php endif; ?>
+
+  </header>
+
+  <div class="single-post-content">
+    <?php the_post_thumbnail('medium_large'); ?>
+    <?php the_content(); ?>
+  </div>
+
+</article>
+
+<?php endwhile; endif; ?>
+
+<?php get_sidebar(); ?>
 </main>
 
-<?php get_footer ();?>  
+<?php get_footer(); ?>
